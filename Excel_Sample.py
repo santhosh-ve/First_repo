3897012e5068831f200a0688f61e6b27bfda4d33
santhosh-ve <67674960+santhosh-ve@users.noreply.github.com>
2020-07-20 15:31:59 +0530

    Update Excel_Sample.py

--- Excel_Sample.py
+++ Excel_Sample.py
@@ -13,15 +13,14 @@ for r in range(2,rows+1):
 	password = XLUtils.readData(path,"Sheet1",r,2)
 	
 	driver.find_element_by_name("userName").send_keys(username)
-  driver.find_element_by_name("password").send_keys(password)
+  	driver.find_element_by_name("password").send_keys(password)
 	driver.find_element_by_name("login").click()
 	
-	 if driver.title == "pagetitle":
-        print("Test Passed!")
-        XLUtils.writeData(path,"Sheet1",2,3,"Test Passed!!")
-    else:
-        print("Test failed")
-		    XLUtils.writeData(path,"Sheet1",2,3,"Test Failed!!")
-        driver.find_element_by_link_text("Home").click()
-
+	if driver.title == "pagetitle":
+        	print("Test Passed!")
+        	XLUtils.writeData(path,"Sheet1",2,3,"Test Passed!!")
+    	else:
+        	print("Test failed")
+		XLUtils.writeData(path,"Sheet1",2,3,"Test Failed!!")
+        	driver.find_element_by_link_text("Home").click()
 driver.close()
