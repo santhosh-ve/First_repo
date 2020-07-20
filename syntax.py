a8732385009e33fcff0fcac2cdddef69bef8a53d
santhosh-ve <67674960+santhosh-ve@users.noreply.github.com>
2020-07-20 18:31:35 +0530

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,55 +1,62 @@
----Packages---
+----Packages----
 
 from selenium import webdriver
 
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditins as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
 
 import time
 import openpyxl
 import logging
 
-***WebDriver***
+----WebDriver----
 
-driver = webdriver.Chrome(executable_path = "",chrome_options=) #To open a browser
-driver.get("http://newtours.demoaut.com/") #To open url
+driver = webdriver.Chrome(executable_path = "",chrome_options=)				#To open a browser
 
-***Browser options***
+driver.get("http://newtours.demoaut.com/") 						#To open url
+
+----Browser options----
+
 from selenium.webdriver.chrome.options import Options
 
 chromeOptions=Options()
-chromeOptions.add_experimental_option("test",{"download.default_directory":"C:\NewFolder"})
-driver = webdriver.Chrome(chrome_options=chromeOptions) 
+chromeOptions.add_experimental_option("pref",{"download.default_directory":"C:\NewFolder"})
+driver = webdriver.Chrome(chrome_options=chromeOptions)  
 
-***headless browser***
+----headless browser----
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 
+print(driver.title) 									#To get the page title
 
-print(driver.title) #To get the page title
-assert "Welcome: Mercury Tours" in driver.title #To verify the title
-print(driver.current_url) #To get the current url
-print(driver.page_source) #To get the HTML content of the page_source
+assert "Welcome: Mercury Tours" in driver.title 					#To verify the title
 
-driver.close() #Close current browser window
-driver.quit() #Close all the windows
+print(driver.current_url) 								#To get the current url
 
-#Boolean 
+print(driver.page_source) 								#To get the HTML content of the page_source
+
+driver.close() 										#Close current browser window
+
+driver.quit() 										#Close all the windows
+
+----Boolean----
 
 is_enabled()
 is_displayed()
 is_selected()
 
-#Navigation
+----Navigation----
 
 driver.back()
 driver.forward()
 
-#Waits
+----Wait----
 #Implicit
 driver.implicitly_wait(5)
 
@@ -67,12 +74,16 @@ for checkbox in multiplecheckbox:
         checkbox.click()
 		
 #No of options
-Element = driver.find_element(By.CLASS_NAME,"dropdown")
-dropdown = Select(Element) # Use select class
-print(len(dropdown.options))
+Element = driver.find_element(By.CLASS_NAME,"")
+dropdown = Select(Element) 				# Use select class to get the dropdown options
+
+print(len(dropdown.options))				# To count the no of options in the dropdown
+
+dropdown.select_by_value("")				# To select a value
 
 #To print the options
-for option in dropdown.options:
+all_options = dropdow.options
+for option in all_options:
     print(option.text)
 
 #Dropdown actions
@@ -88,36 +99,43 @@ driver.switch_to.alert.accept()
 driver.switch_to_frame("classFrame")
 
 #Browser window
-driver.current_window_handle
-driver.window_handles
+driver.current_window_handle				#Returns the handle of the current window
+driver.window_handles					#Returns the handles of all windows within the current session. 
 
 #Scrolling
-Scrolldown the page by pixel
-driver.execute_script("window.scrollBy(0,500)","")
+#Scrolldown the page by pixel
+driver.execute_script("window.scrollBy(0,500)","")	#It uses JavaScriptExecutor
 
-Scrolldown the page till element found
+#Scrolldown the page till element found
 driver.execute_script("arguments[0].scrollintoView();",Element)
 
-Scroll to end of the page
+#Scroll to end of the page
 driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
 
-
 #Actions
-actions = ActionChains(driver)
-actions.move_to_element().click().perform()
+from selenium.webdriver import ActionChains  
+action_chains = ActionChains(driver) 
 
-available actions 
+source = driver.find_element_by_name("source")  
+target = driver.find_element_by_name("target")  
+
+action_chains.drag_and_drop(source, target).perform() 
+
+#available actions 
 drag_and_drop()
 click_and_hold()
 double_click()
 context_click()
 
 #To upload a file 
+filepath = "specifythefilepath"
 driver.find_element("sample").send_keys("filepath")
 
+#ScreenShots
+driver.save_screenshot("filename")
+driver.get_screenshot_as_file("filename")
 
 #Connect to db
-
 import cx_Oracle
 import os
 
